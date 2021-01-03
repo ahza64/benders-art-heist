7fb6fcb6988614109b701f19469e0ad159ce0b98
ahza64 <justinmichaelpettit@gmail.com>
2021-01-03 15:28:20 -0800

    first react hooks app, getting state up and running

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+
+  const [search, setSearch] = useState('enter here');
+
+  function updateSearch(e) {
+    console.log("updateSearch", e.target.value);
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" onChange={e => updateSearch(e)} value={search}/>
+
     </div>
   );
+
 }
 
 export default App;
